d3da7763a13334259315d9f7eedd91b0e92572f4
komp1474 <akinaksay1@gmail.com>
2023-12-14 22:19:47 -0500

    Update measure-webserver.py

--- measure-webserver.py
+++ measure-webserver.py
@@ -5,7 +5,6 @@ from scapy.layers import http
 from scapy.layers.inet import IP, TCP
 import sys
 import math
-import numpy as np
 
 load_layer("http")
 latencies = []
@@ -29,6 +28,67 @@ def calculate_percentiles():
     percents.append(percentile(0.99))
     return percents
 
+def measured_distribution():
+    min_latency = min(latencies)
+    max_latency = max(latencies)
+    bucket_size = (max_latency - min_latency) / 10
+
+    bucket_counts = [0] * 10
+    for latency in latencies:
+        bucket_index = min(int((latency - min_latency) / bucket_size), 9)
+        bucket_counts[bucket_index] += 1
+
+    total_measurements = len(latencies)
+    measured_distribution = [count / total_measurements for count in bucket_counts]
+    print(measured_distribution)
+    return measured_distribution
+
+
+
+def exponential_model_distribution():
+    mean_response_time = get_avg()
+    rate_parameter = 1.0 / float(mean_response_time)
+
+    min_latency = min(latencies)
+    max_latency = max(latencies)
+    bucket_size = (max_latency - min_latency) / 10
+
+    bucket_boundaries = [i * bucket_size for i in range(11)]
+    exponential_distribution = []
+
+    for i in range(9):
+        lower_boundary = bucket_boundaries[i] * (1.0 / float(bucket_size))
+        upper_boundary = bucket_boundaries[i + 1] * (1.0 / float(bucket_size))
+
+        cdf_lower_boundary = 1 - math.exp(-rate_parameter * lower_boundary)
+        cdf_upper_boundary = 1 - math.exp(-rate_parameter * upper_boundary)
+
+        probability_mass = cdf_upper_boundary - cdf_lower_boundary
+        exponential_distribution.append(probability_mass)
+
+    # Last bucket calculation
+    last_upper_boundary = bucket_boundaries[9] * (1.0 / float(bucket_size))
+    cdf_last_upper_boundary = 1 - math.exp(-rate_parameter * last_upper_boundary)
+    probability_mass_last_bucket = 1 - cdf_last_upper_boundary
+    exponential_distribution.append(probability_mass_last_bucket)
+
+    return exponential_distribution
+
+
+
+def kl_divergence(dist1, dist2):
+    if len(dist1) != len(dist2):
+        raise ValueError("WRONG")
+    
+    kl_sum = 0.0
+    for i in range(len(dist1)):
+
+        if((dist1[i]==0) or (dist2[i] == 0)):
+            continue
+
+        kl_sum = kl_sum +(dist1[i] * math.log2(dist1[i]/dist2[i]))
+
+    return kl_sum
 
 def measure_webserver(input_file, server_ip, server_port):
 
@@ -57,11 +117,11 @@ def measure_webserver(input_file, server_ip, server_port):
             latencies.append(response.time - request.time)
 
     if(len(latencies) != 0):
+        kl_div =  kl_divergence(exponential_model_distribution(), measured_distribution())
 
         print(f"AVERAGE LATENCY: {get_avg():.5f}")
         print(f"PERCENTILES: {calculate_percentiles()[0]:.5f}, {calculate_percentiles()[1]:.5f}, {calculate_percentiles()[2]:.5f}, {calculate_percentiles()[3]:.5f}, {calculate_percentiles()[4]:.5f}")
-        #print(f"KL DIVERGENCE: {kl_div:.5f}")
-
+        print(f"KL DIVERGENCE: {kl_div:.5f}")
 if __name__ == "__main__":
 
     input_file = sys.argv[1]
